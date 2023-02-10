063079ee0ff2005c1de11a02bb27e707a45c4bee
Milad Zahmatkesh <mizahma@siue.edu>
2023-02-10 01:37:45 -0600

    score limitation,add playagain alert

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('game');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("game");
+const context = canvas.getContext("2d");
 const grid = 15;
 const paddleHeight = grid * 5; // 80
 const maxPaddleY = canvas.height - grid - paddleHeight;
@@ -18,7 +18,7 @@ const leftPaddle = {
   height: paddleHeight,
 
   // paddle velocity
-  dy: 0
+  dy: 0,
 };
 const rightPaddle = {
   // start in the middle of the game on the right side
@@ -28,7 +28,7 @@ const rightPaddle = {
   height: paddleHeight,
 
   // paddle velocity
-  dy: 0
+  dy: 0,
 };
 const ball = {
   // start in the middle of the game
@@ -42,118 +42,128 @@ const ball = {
 
   // ball velocity (start going to the top-right corner)
   dx: ballSpeed,
-  dy: -ballSpeed
+  dy: -ballSpeed,
 };
 
 // check for collision between two objects using axis-aligned bounding box (AABB)
 // @see https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 function collides(obj1, obj2) {
-  return obj1.x < obj2.x + obj2.width &&
-         obj1.x + obj1.width > obj2.x &&
-         obj1.y < obj2.y + obj2.height &&
-         obj1.y + obj1.height > obj2.y;
+  return (
+    obj1.x < obj2.x + obj2.width &&
+    obj1.x + obj1.width > obj2.x &&
+    obj1.y < obj2.y + obj2.height &&
+    obj1.y + obj1.height > obj2.y
+  );
 }
 
 // game loop
 function loop() {
-  requestAnimationFrame(loop);
-  context.clearRect(0,0,canvas.width,canvas.height);
+  if (leftScore > 6 || rightScore > 6) {
+    document.querySelector("#alert").style.display="block"
+  } else {
+    requestAnimationFrame(loop);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // move paddles by their velocity
-  leftPaddle.y += leftPaddle.dy;
-  rightPaddle.y += rightPaddle.dy;
+    // move paddles by their velocity
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
 
-  // prevent paddles from going through walls
-  if (leftPaddle.y < grid) {
-    leftPaddle.y = grid;
-  }
-  else if (leftPaddle.y > maxPaddleY) {
-    leftPaddle.y = maxPaddleY;
-  }
-
-  if (rightPaddle.y < grid) {
-    rightPaddle.y = grid;
-  }
-  else if (rightPaddle.y > maxPaddleY) {
-    rightPaddle.y = maxPaddleY;
-  }
-
-  // draw paddles
-  context.fillStyle = 'white';
-  context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-  context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
-
-  // move ball by its velocity
-  ball.x += ball.dx;
-  ball.y += ball.dy;
-
-  // prevent ball from going through walls by changing its velocity
-  if (ball.y < grid) {
-    ball.y = grid;
-    ball.dy *= -1;
-  }
-  else if (ball.y + grid > canvas.height - grid) {
-    ball.y = canvas.height - grid * 2;
-    ball.dy *= -1;
-  }
-
-  // reset ball if it goes past paddle (but only if we haven't already done so)
-  if ( (ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
-    ball.resetting = true;
-    //keep score
-    if (ball.x < 0) {
-      rightScore = rightScore + 1;
+    // prevent paddles from going through walls
+    if (leftPaddle.y < grid) {
+      leftPaddle.y = grid;
+    } else if (leftPaddle.y > maxPaddleY) {
+      leftPaddle.y = maxPaddleY;
     }
-    else if (ball.x > canvas.width) {
-      leftScore = leftScore + 1;
+
+    if (rightPaddle.y < grid) {
+      rightPaddle.y = grid;
+    } else if (rightPaddle.y > maxPaddleY) {
+      rightPaddle.y = maxPaddleY;
     }
-    // give some time for the player to recover before launching the ball again
-    setTimeout(() => {
-      ball.resetting = false;
-      ball.x = canvas.width / 2;
-      ball.y = canvas.height / 2;
-    }, 400);
+
+    // draw paddles
+    context.fillStyle = "white";
+    context.fillRect(
+      leftPaddle.x,
+      leftPaddle.y,
+      leftPaddle.width,
+      leftPaddle.height
+    );
+    context.fillRect(
+      rightPaddle.x,
+      rightPaddle.y,
+      rightPaddle.width,
+      rightPaddle.height
+    );
+
+    // move ball by its velocity
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // prevent ball from going through walls by changing its velocity
+    if (ball.y < grid) {
+      ball.y = grid;
+      ball.dy *= -1;
+    } else if (ball.y + grid > canvas.height - grid) {
+      ball.y = canvas.height - grid * 2;
+      ball.dy *= -1;
+    }
+
+    // reset ball if it goes past paddle (but only if we haven't already done so)
+    if ((ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
+      ball.resetting = true;
+      //keep score
+      if (ball.x < 0) {
+        rightScore = rightScore + 1;
+      } else if (ball.x > canvas.width) {
+        leftScore = leftScore + 1;
+      }
+      // give some time for the player to recover before launching the ball again
+      setTimeout(() => {
+        ball.resetting = false;
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+      }, 400);
+    }
+
+    // check to see if ball collides with paddle. if they do change x velocity
+    if (collides(ball, leftPaddle)) {
+      ball.dx *= -1;
+
+      // move ball next to the paddle otherwise the collision will happen again
+      // in the next frame
+      ball.x = leftPaddle.x + leftPaddle.width;
+    } else if (collides(ball, rightPaddle)) {
+      ball.dx *= -1;
+
+      // move ball next to the paddle otherwise the collision will happen again
+      // in the next frame
+      ball.x = rightPaddle.x - ball.width;
+    }
+
+    // draw ball
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    // draw walls
+    context.fillStyle = "lightgrey";
+    context.fillRect(0, 0, canvas.width, grid);
+    context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
+
+    // draw dotted line down the middle
+    for (let i = grid; i < canvas.height - grid; i += grid * 2) {
+      context.fillRect(canvas.width / 2 - grid / 2, i, grid, grid);
+    }
+
+    // print score
+    context.font = "50px serif";
+    context.fillText(leftScore, 300, 350, 50);
+    context.font = "50px serif";
+    context.fillText(rightScore, 400, 350, 50);
   }
-
-  // check to see if ball collides with paddle. if they do change x velocity
-  if (collides(ball, leftPaddle)) {
-    ball.dx *= -1;
-
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
-    ball.x = leftPaddle.x + leftPaddle.width;
-  }
-  else if (collides(ball, rightPaddle)) {
-    ball.dx *= -1;
-
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
-    ball.x = rightPaddle.x - ball.width;
-  }
-
-  // draw ball
-  context.fillRect(ball.x, ball.y, ball.width, ball.height);
-
-  // draw walls
-  context.fillStyle = 'lightgrey';
-  context.fillRect(0, 0, canvas.width, grid);
-  context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
-
-  // draw dotted line down the middle
-  for (let i = grid; i < canvas.height - grid; i += grid * 2) {
-    context.fillRect(canvas.width / 2 - grid / 2, i, grid, grid);
-  }
-
-  // print score
-  context.font = "50px serif";
-  context.fillText(leftScore, 300, 350, 50);
-  context.font = "50px serif";
-  context.fillText(rightScore, 400, 350, 50);
 }
 
 // listen to keyboard events to move the paddles
-document.addEventListener('keydown', function(e) {
-
+document.addEventListener("keydown", function (e) {
   // up arrow key
   if (e.which === 38) {
     rightPaddle.dy = -paddleSpeed;
@@ -174,7 +184,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 // listen to keyboard events to stop the paddle if key is released
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function (e) {
   if (e.which === 38 || e.which === 40) {
     rightPaddle.dy = 0;
   }
@@ -183,6 +193,14 @@ document.addEventListener('keyup', function(e) {
     leftPaddle.dy = 0;
   }
 });
+document.querySelector("#close-btn").addEventListener("click",function(e) {
+  e.target.parentElement.style.display="none";
+})
+document.querySelector("#playAgain-btn").addEventListener("click",function(e) {
+  leftScore=rightScore=0;
+  e.target.parentElement.style.display="none";
+  requestAnimationFrame(loop);
+})
 
 // start the game
 requestAnimationFrame(loop);
